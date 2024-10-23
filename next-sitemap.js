3039ba9a3c0f4d20325bd4e87f4a6645954d7624
Luis <ltracam0206@iesmartinezm,es>
2024-10-23 21:17:57 +0200

    Actualizar favicon.svg y eliminar robots.txt y sitemap.xml

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,5 +1,16 @@
-module.exports = {
-    siteUrl: 'https://www.anedac.es',
-    generateRobotsTxt: true, // Opcional, para generar robots.txt también
-  }
-  
+const config = {
+	siteUrl: 'https://www.anedac.es',
+	generateRobotsTxt: true,
+	robotsTxtOptions: {
+		policies: [
+			{ userAgent: '*', allow: '/' },
+			{ userAgent: '*', disallow: '/admin' }
+		]
+	},
+	changefreq: 'daily',
+	priority: 0.7,
+	exclude: ['/admin/*', '/private/*'],
+	additionalPaths: async (config) => ['/blog/*', '/src/pages/*', '/src/content/blog/*']
+}
+
+export default config
