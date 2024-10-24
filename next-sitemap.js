69dcb231fcc5400b1a9347f5a51b5b694e8a177c
Luis <ltracam0206@iesmartinezm,es>
2024-10-24 16:59:03 +0200

    Revert "Actualizar favicon.svg y eliminar robots.txt y sitemap.xml"
    This reverts commit 3039ba9a3c0f4d20325bd4e87f4a6645954d7624.

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,16 +1,6 @@
-const config = {
-	siteUrl: 'https://www.anedac.es',
-	generateRobotsTxt: true,
-	robotsTxtOptions: {
-		policies: [
-			{ userAgent: '*', allow: '/' },
-			{ userAgent: '*', disallow: '/admin' }
-		]
-	},
-	changefreq: 'daily',
-	priority: 0.7,
-	exclude: ['/admin/*', '/private/*'],
-	additionalPaths: async (config) => ['/blog/*', '/src/pages/*', '/src/content/blog/*']
-}
+import { defineConfig } from 'astro/config'
+import sitemap from '@astrojs/sitemap'
 
-export default config
+export default defineConfig({
+	integrations: [sitemap()]
+})
