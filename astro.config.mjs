bf00658ddb5848f83967b1728339939a5dd1b9fd
Luis <ltracam0206@iesmartinezm,es>
2025-08-25 15:04:10 +0200

    Agregar dependencia de Partytown y actualizar configuración de integraciones en Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,33 +1,32 @@
-import { defineConfig } from 'astro/config'
-import mdx from '@astrojs/mdx'
-import sitemap from '@astrojs/sitemap'
-import tailwind from '@astrojs/tailwind'
-import { remarkReadingTime } from './src/utils/readTime.ts'
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
+import { remarkReadingTime } from './src/utils/readTime.ts';
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://anedac.es', // Write here your website url
-	markdown: {
-		remarkPlugins: [remarkReadingTime],
-		drafts: true,
-		shikiConfig: {
-			theme: 'material-theme-palenight',
-			wrap: true
-		}
-	},
-	integrations: [
-		mdx({
-			syntaxHighlight: 'shiki',
-			shikiConfig: {
-				experimentalThemes: {
-					light: 'vitesse-light',
-					dark: 'material-theme-palenight',
-				  },
-				wrap: true
-			},
-			drafts: true
-		}),
-		sitemap(),
-		tailwind()
-	]
-})
+  site: 'https://anedac.es',
+  // Write here your website url
+  markdown: {
+    remarkPlugins: [remarkReadingTime],
+    drafts: true,
+    shikiConfig: {
+      theme: 'material-theme-palenight',
+      wrap: true
+    }
+  },
+  integrations: [mdx({
+    syntaxHighlight: 'shiki',
+    shikiConfig: {
+      experimentalThemes: {
+        light: 'vitesse-light',
+        dark: 'material-theme-palenight'
+      },
+      wrap: true
+    },
+    drafts: true
+  }), sitemap(), tailwind(), partytown()]
+});
